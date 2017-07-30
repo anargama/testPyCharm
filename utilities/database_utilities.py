09e0e4424c1b921759654e08c08d97788920c4ec
Marina <ma2748@columbia.edu>
2017-07-30 16:20:25 -0400

    initial commit

--- utilities/database_utilities.py
+++ utilities/database_utilities.py
@@ -1,5 +1,5 @@
 def create_database(database_path: str):
-    #TODO: generate the database
+    #TODO: generate the database bbb
     pass
 
 def save_words_to_database(database_path:str, words_list: list):
